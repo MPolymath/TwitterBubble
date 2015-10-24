02889e634a98f4ed2922198ff6a783cbbd5b68e1
Massamba DIOUF <mdiouf@e3r12p2.42.fr>
2015-10-24 19:20:40 +0200

    Chat with messages to console (socket.io tut)

--- Socket/index.js
+++ Socket/index.js
@@ -1,4 +1,3 @@
-
 var app = require('express')();
 var http = require('http').Server(app);
 var io =  require('socket.io')(http);
@@ -8,9 +7,12 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-	console.log('a user connected');
-	socket.on('disconnect', function() {
-		console.log('user disconnected');
+//	console.log('a user connected');
+//	socket.on('disconnect', function() {
+//		console.log('user disconnected');
+	//	});
+	socket.on('chat message', function(msg){
+		console.log('message: ' +msg);
 	});
 });
 
